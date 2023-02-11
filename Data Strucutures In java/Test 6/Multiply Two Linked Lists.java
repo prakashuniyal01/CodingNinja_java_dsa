68d601eb952eecc9f6aa90e04424373c02e656f5
prakash uniyal <uniyalprakash7@gmail.com>
2023-02-11 20:48:04 +0530

    coding ninja java solution

---
+++ Data Strucutures In java/Test 6/Multiply Two Linked Lists.java
@@ -0,0 +1,19 @@
+// Multiply Two Linked Lists
+// Send Feedback
+// 1. You are given two linked lists. Data of each node of these linked lists contain a digit from the range: [0, 9].
+// 2. The linked lists represent two numbers.
+// 3. You have to print the product of these two numbers.
+// Input Format:
+// Input has already been managed for cpp and java submissions. In cpp and java, you have to just write a function which receives head nodes of two linked lists. The sizes of linked lists are N and M, respectively.
+// For other languages, the first line of input contains list of N space separated integers, terminated by -1. The following line of input also contains list of M space separated integers, terminated by -1. The integers form the data of nodes of two linked list.   
+// Constraints:
+// N and M lies in the range: [1, 1000].
+// 0 <= |Node Data| <= 9
+// Output format:
+// Print the product of numbers, represented by two linked lists.
+// Sample Input:
+// 1 2 3 4 5 -1
+// 1 2 3 -1
+// Sample Output:
+// 1 5 1 8 4 3 5
+
